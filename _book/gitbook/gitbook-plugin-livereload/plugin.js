228a8bbcdbfa404442df86baa0932ab1c6acf2da
ofio <502469688@qq.com>
2019-05-18 16:58:11 +0800

    modify live reload

--- _book/gitbook/gitbook-plugin-livereload/plugin.js
+++ _book/gitbook/gitbook-plugin-livereload/plugin.js
@@ -1,11 +1,11 @@
-(function() {
-  var newEl = document.createElement('script'),
-      firstScriptTag = document.getElementsByTagName('script')[0];
+//(function() {
+//  var newEl = document.createElement('script'),
+//      firstScriptTag = document.getElementsByTagName('script')[0];
 
-  if (firstScriptTag) {
+//  if (firstScriptTag) {
 //    newEl.async = 1;
 //    newEl.src = '//' + window.location.hostname + ':35729/livereload.js';
 //    firstScriptTag.parentNode.insertBefore(newEl, firstScriptTag);
-  }
+//  }
 
-})();
+//})();
