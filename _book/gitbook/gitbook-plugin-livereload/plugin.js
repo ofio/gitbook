903e2003dc78ac8b5e334cbd1838610005c69298
ofio <502469688@qq.com>
2019-05-18 16:42:36 +0800

    livereload.js

--- _book/gitbook/gitbook-plugin-livereload/plugin.js
+++ _book/gitbook/gitbook-plugin-livereload/plugin.js
@@ -3,9 +3,9 @@
       firstScriptTag = document.getElementsByTagName('script')[0];
 
   if (firstScriptTag) {
-    newEl.async = 1;
-    newEl.src = '//' + window.location.hostname + ':35729/livereload.js';
-    firstScriptTag.parentNode.insertBefore(newEl, firstScriptTag);
+//    newEl.async = 1;
+//    newEl.src = '//' + window.location.hostname + ':35729/livereload.js';
+//    firstScriptTag.parentNode.insertBefore(newEl, firstScriptTag);
   }
 
 })();
